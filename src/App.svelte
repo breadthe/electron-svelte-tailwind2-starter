e0b3da5bce9e6536d0db6d2798e3ce3abb9ca09e
breadthe <omigoshdev@protonmail.com>
2021-02-18 07:48:40 -0600

    Fix centering

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
     "blue",
     "indigo",
     "purple",
-    "pink"
+    "pink",
   ];
   const shades = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
@@ -42,6 +42,51 @@
   }
 </script>
 
+<!-- Note: "class:dark" is equivalent (and short for) "class:dark={dark}" or "class:dark={dark === true}" -->
+<main
+  class="bg-gradient-to-br min-h-screen flex items-center justify-center"
+  class:dark
+  class:from-blue-700={$dark}
+  class:to-purple-800={$dark}
+  class:from-yellow-200={!$dark}
+  class:to-pink-300={!$dark}
+>
+  <div class="flex flex-col items-center space-y-8">
+    <div class:text-pink-100={$dark} class:text-pink-900={!$dark}>
+      <h1 class="hello leading-tight">{title}</h1>
+      Visit the
+      <a
+        href="https://svelte.dev/tutorial"
+        class="border-b-4 border-dashed border-opacity-30
+        hover:border-opacity-100"
+        class:border-pink-100={$dark}
+        class:border-pink-900={!$dark}
+      >
+        Svelte tutorial
+      </a>
+      to learn how to build Svelte apps.
+    </div>
+    <div class="flex items-center">
+      <AppearanceToggler />
+    </div>
+    <div
+      class="border-4 border-dashed max-w-sm p-4 rounded-lg space-y-2 {`border-${color}-${shade}`}"
+    >
+      <p class:text-pink-100={$dark} class:text-pink-900={!$dark}>
+        This next button demonstrates the use of
+        <span class="highlight">@apply</span>
+        . See
+        <span class="highlight">global.css</span>
+        for examples.
+      </p>
+      <button type="button" class="custom-btn" on:click={() => clickMe()}>
+        Click Me
+      </button>
+    </div>
+  </div>
+</main>
+
+<!-- Note: "class:dark" is equivalent (and short for) "class:dark={dark}" or "class:dark={dark === true}" -->
 <style>
   main {
     text-align: center;
@@ -56,44 +101,3 @@
     }
   }
 </style>
-
-<!-- Note: "class:dark" is equivalent (and short for) "class:dark={dark}" or "class:dark={dark === true}" -->
-<main
-  class="bg-gradient-to-br"
-  class:dark
-  class:from-blue-700={$dark}
-  class:to-purple-800={$dark}
-  class:from-yellow-200={!$dark}
-  class:to-pink-300={!$dark}>
-  <div class="flex flex-col items-center justify-center min-h-screen space-y-8">
-    <div class:text-pink-100={$dark} class:text-pink-900={!$dark}>
-      <h1 class="hello leading-tight">{title}</h1>
-      Visit the
-      <a
-        href="https://svelte.dev/tutorial"
-        class="border-b-4 border-dashed border-opacity-30
-        hover:border-opacity-100"
-        class:border-pink-100={$dark}
-        class:border-pink-900={!$dark}>
-        Svelte tutorial
-      </a>
-      to learn how to build Svelte apps.
-    </div>
-    <div class="flex items-center">
-      <AppearanceToggler />
-    </div>
-    <div
-      class="border-4 border-dashed max-w-sm p-4 rounded-lg space-y-2 {`border-${color}-${shade}`}">
-      <p class:text-pink-100={$dark} class:text-pink-900={!$dark}>
-        This next button demonstrates the use of
-        <span class="highlight">@apply</span>
-        . See
-        <span class="highlight">global.css</span>
-        for examples.
-      </p>
-      <button type="button" class="custom-btn" on:click={() => clickMe()}>
-        Click Me
-      </button>
-    </div>
-  </div>
-</main>
